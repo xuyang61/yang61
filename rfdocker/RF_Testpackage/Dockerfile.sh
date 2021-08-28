5e097623a3b8d70601efbbcb6ec9264a067bad68
Python_Robot <68461556+xuyang61@users.noreply.github.com>
2021-08-28 16:13:21 +0300

    Delete Dockerfile.sh

--- rfdocker/RF_Testpackage/Dockerfile.sh
+++
@@ -1,7 +0,0 @@
-# https://github.com/xuyang61/rfdocker
-# https://hub.docker.com/r/robotframework/rfdocker
-
-FROM robotframework/rfdocker:4.0.2
-
-COPY requirements.txt .
-RUN pip install --no-cache-dir -r requirements.txt
